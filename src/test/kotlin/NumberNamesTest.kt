dfc20082af1fb804e15a4a2b1aeb6067395aa6be
Luis Davi Campos <luisdavicampos@usp.br>
2022-06-07 17:48:05 -0300

    red: create unity test `testa 2`

--- src/test/kotlin/NumberNamesTest.kt
+++ src/test/kotlin/NumberNamesTest.kt
@@ -6,4 +6,8 @@ class NumberNamesTest {
     fun `testa 1`() {
         assertEquals("one", numberNames(1))
     }
+    @Test
+    fun `testa 2`() {
+        assertEquals("two", numberNames(2))
+    }
 }
