39c2a1589e9bee9118c736e834356e60f94c8fda
Luis Davi Campos <luisdavicampos@usp.br>
2022-06-08 11:06:13 -0300

    refactor: create values to store unit and ten digits
    Also correct the name of 'dozens' array to 'tens'

--- src/main/kotlin/NumberNames.kt
+++ src/main/kotlin/NumberNames.kt
@@ -1,8 +1,16 @@
 fun numberNames (n : Int): String {
     val units = arrayOf("zero", "one", "two", "three",
         "four", "five", "six", "seven", "eight", "nine")
-    val dozens = arrayOf("ten", "eleven", "twelve", "thirteen", "fourteen",
+    val tens = arrayOf("ten", "eleven", "twelve", "thirteen", "fourteen",
         "fifhteen", "seventeen", "eighteen", "nineteen")
-    return if (n < 10) units[n]
-    else dozens[n%10]
+
+    val unit = n % 10     // units digit
+    val ten = n / 10      // tens digit
+
+    return if (ten == 0) {
+        units[unit]
+    }
+    else {
+        tens[unit]
+    }
 }
