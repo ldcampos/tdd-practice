1a926d4c9a1fc67a5a16505d244d11a19673bc42
Luis Davi Campos <luisdavicampos@usp.br>
2022-06-08 10:40:32 -0300

    green: makes numberNames pass previous test
    Works for numbers from 0 (zero) to 19 (nineteen)

--- src/main/kotlin/NumberNames.kt
+++ src/main/kotlin/NumberNames.kt
@@ -1,5 +1,8 @@
 fun numberNames (n : Int): String {
-    val numbers = arrayOf("zero", "one", "two", "three",
-        "four", "five", "six", "seven", "eight", "nine", "ten")
-    return numbers[n]
+    val units = arrayOf("zero", "one", "two", "three",
+        "four", "five", "six", "seven", "eight", "nine")
+    val dozens = arrayOf("ten", "eleven", "twelve", "thirteen", "fourteen",
+        "fifhteen", "seventeen", "eighteen", "nineteen")
+    return if (n < 10) units[n]
+    else dozens[n%10]
 }
