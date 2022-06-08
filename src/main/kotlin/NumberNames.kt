eca527797806089d6aae2117d1ad76b3c918976c
Luis Davi Campos <luisdavicampos@usp.br>
2022-06-08 12:38:54 -0300

    refactor: improve code legibility in numberNames
    Change Arrays indentation, replace "return when" by "return if" and add comments to explain ifs

--- src/main/kotlin/NumberNames.kt
+++ src/main/kotlin/NumberNames.kt
@@ -1,26 +1,26 @@
-fun numberNames (n : Int): String {
-    val units = arrayOf("zero", "one", "two", "three",
-        "four", "five", "six", "seven", "eight", "nine")
-    val teens = arrayOf("ten", "eleven", "twelve", "thirteen", "fourteen",
-        "fifhteen", "seventeen", "eighteen", "nineteen")
-    val tens = arrayOf("zero", "ten", "twenty", "thirty", "forty", "fifhty",
-        "sixty", "seventy", "eighty", "ninety")
+fun numberNames(n: Int): String {
+    val units = arrayOf(
+        "zero", "one", "two", "three",
+        "four", "five", "six", "seven", "eight", "nine"
+    )
+    val teens = arrayOf(
+        "ten", "eleven", "twelve", "thirteen", "fourteen",
+        "fifhteen", "seventeen", "eighteen", "nineteen"
+    )
+    val tens = arrayOf(
+        "zero", "ten", "twenty", "thirty", "forty", "fifhty",
+        "sixty", "seventy", "eighty", "ninety"
+    )
 
     val unit = n % 10     // units digit
     val ten = n / 10      // tens digit
 
-    return when (ten) {
-        0 -> {
-            units[unit]
-        }
-        1 -> {
-            teens[unit]
-        }
-        else -> {
-            val tensName = tens[ten]
-
-            if (unit>0) "$tensName ${units[unit]}"
-            else tensName
-        }
-    }
+    return if (unit == 0)               // round tens
+        tens[ten]
+    else if (ten == 0)                  // units
+        units[unit]
+    else if (ten == 1)                  // numbers between 11-19
+        teens[unit]
+    else                                // numbers from 21 to 99
+        "${tens[ten]} ${units[unit]}"
 }
