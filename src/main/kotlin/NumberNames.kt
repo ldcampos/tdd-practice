b8e22a66de5f4b06d6a91b71fa546f35ce3b5f4b
Luis Davi Campos <luisdavicampos@usp.br>
2022-06-10 17:56:27 -0300

    refactor: rearrange ifs and improve indentation

--- src/main/kotlin/NumberNames.kt
+++ src/main/kotlin/NumberNames.kt
@@ -1,30 +1,31 @@
 fun numberNames(n: Int): String {
     val units = arrayOf(
-        "zero", "one", "two", "three",
-        "four", "five", "six", "seven", "eight", "nine"
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"
     )
     val teens = arrayOf(
         "ten", "eleven", "twelve", "thirteen", "fourteen",
         "fifhteen", "seventeen", "eighteen", "nineteen"
     )
     val tens = arrayOf(
-        "zero", "ten", "twenty", "thirty", "forty", "fifhty",
-        "sixty", "seventy", "eighty", "ninety"
+        "zero", "ten", "twenty", "thirty", "forty",
+        "fifhty", "sixty", "seventy", "eighty", "ninety"
     )
 
     val unit = n % 10         // units digit
     val ten = n / 10          // tens digit
     val hundred = n / 100     // hundred digit
 
-    return if (hundred > 0)             // hundreds
-        "${units[hundred]} hundred" +
-                (if (unit>0) " and ${units[unit]}" else "")
-    else if (unit == 0)                 // round tens
-        tens[ten]
-    else if (ten == 0)                  // units
+    return if (ten == 0)                             // n < 10
         units[unit]
-    else if (ten == 1)                  // numbers between 11-19
+    else if (ten == 1)                               // 10 < n <= 19
         teens[unit]
-    else                                // numbers from 21 to 99
-        "${tens[ten]} ${units[unit]}"
+    else if (ten <= 9)                               // 20 <= n < 100
+        tens[ten] +
+                if (unit > 0) " ${units[unit]}"
+                else ""
+    else                                             // n >= 100
+        units[hundred] + " hundred" +
+                if (unit > 0) " and ${units[unit]}"
+                else ""
 }
