00a9656a448b8b4540b9665b037d387a4e2d7983
Luis Davi Campos <luisdavicampos@usp.br>
2022-06-16 12:26:06 -0300

    refactor: create class NumberNames

--- src/main/kotlin/NumberNames.kt
+++ src/main/kotlin/NumberNames.kt
@@ -1,49 +1,51 @@
-/* list of 'atomic' names */
+class NumberNames {
+    /* lists of 'atomic' names */
+    private val units = listOf(
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"
+    )
+    private val teens = listOf(
+        "ten", "eleven", "twelve", "thirteen", "fourteen",
+        "fifhteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    )
+    private val tens = listOf(
+        "zero", "ten", "twenty", "thirty", "forty",
+        "fifhty", "sixty", "seventy", "eighty", "ninety"
+    )
 
-val units = listOf(
-    "zero", "one", "two", "three", "four",
-    "five", "six", "seven", "eight", "nine"
-)
-val teens = listOf(
-    "ten", "eleven", "twelve", "thirteen", "fourteen",
-    "fifhteen", "sixteen", "seventeen", "eighteen", "nineteen"
-)
-val tens = listOf(
-    "zero", "ten", "twenty", "thirty", "forty",
-    "fifhty", "sixty", "seventy", "eighty", "ninety"
-)
+    private fun nameUnits(n: Int): String {
+        val unitsDigit = n % 10
+        val tensDigit = (n % 100) / 10
 
-fun nameUnits(n : Int) : String {
-    val unitsDigit = n % 10
-    val tensDigit = (n % 100) / 10
+        return if ((unitsDigit != 0 || n == 0) && tensDigit != 1)
+            units[unitsDigit]
+        else ""
+    }
 
-    return if ((unitsDigit != 0 || n == 0) && tensDigit != 1)
-        units[unitsDigit]
-    else ""
-}
-fun nameTens(n : Int) : String {
-    val unitsDigit = n % 10
+    private fun nameTens(n: Int): String {
+        val unitsDigit = n % 10
 
-    return when (val tensDigit = (n % 100) / 10) {
-        0 -> ""
-        1 -> teens[unitsDigit]
-        else -> {
-            tens[tensDigit] +
-            if (unitsDigit != 0) " "
-            else ""
+        return when (val tensDigit = (n % 100) / 10) {
+            0 -> ""
+            1 -> teens[unitsDigit]
+            else -> {
+                tens[tensDigit] +
+                        if (unitsDigit != 0) " "
+                        else ""
+            }
         }
     }
-}
 
-fun nameHundreds(n : Int) : String {
-    val hundredsDigit = n / 100
+    private fun nameHundreds(n: Int): String {
+        val hundredsDigit = n / 100
 
-    return if (hundredsDigit == 0) ""
-    else
-        units[hundredsDigit] + " hundred" +
-            (if (n % 100 != 0) " and " else "")
-}
+        return if (hundredsDigit == 0) ""
+        else
+            units[hundredsDigit] + " hundred" +
+                    (if (n % 100 != 0) " and " else "")
+    }
 
-fun numberNames(n: Int): String {
-    return nameHundreds(n) + nameTens(n) + nameUnits(n)
+    fun numberNames(n: Int): String {
+        return nameHundreds(n) + nameTens(n) + nameUnits(n)
+    }
 }
--- src/test/kotlin/NumberNamesTest.kt
+++ src/test/kotlin/NumberNamesTest.kt
@@ -2,96 +2,98 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class NumberNamesTest {
+    private val testNames: NumberNames = NumberNames()
+
     @Test
     fun `testa 1`() {
-        assertEquals("one", numberNames(1))
+        assertEquals("one", testNames.numberNames(1))
     }
     @Test
     fun `testa 2`() {
-        assertEquals("two", numberNames(2))
+        assertEquals("two", testNames.numberNames(2))
     }
     @Test
     fun `testa 3`() {
-        assertEquals("three", numberNames(3))
+        assertEquals("three", testNames.numberNames(3))
     }
     @Test
     fun `testa 4`() {
-        assertEquals("four", numberNames(4))
+        assertEquals("four", testNames.numberNames(4))
     }
     @Test
     fun `testa 5`() {
-        assertEquals("five", numberNames(5))
+        assertEquals("five", testNames.numberNames(5))
     }
     @Test
     fun `testa 10`() {
-        assertEquals("ten", numberNames(10))
+        assertEquals("ten", testNames.numberNames(10))
     }
     @Test
     fun `testa 11`() {
-        assertEquals("eleven", numberNames(11))
+        assertEquals("eleven", testNames.numberNames(11))
     }
     @Test
     fun `testa 20`() {
-        assertEquals("twenty", numberNames(20))
+        assertEquals("twenty", testNames.numberNames(20))
     }
     @Test
     fun `testa 21`() {
-        assertEquals("twenty one", numberNames(21))
+        assertEquals("twenty one", testNames.numberNames(21))
     }
     @Test
     fun `testa 30`() {
-        assertEquals("thirty", numberNames(30))
+        assertEquals("thirty", testNames.numberNames(30))
     }
     @Test
     fun `testa 31`() {
-        assertEquals("thirty one", numberNames(31))
+        assertEquals("thirty one", testNames.numberNames(31))
     }
     @Test
     fun `testa 100`() {
-        assertEquals("one hundred", numberNames(100))
+        assertEquals("one hundred", testNames.numberNames(100))
     }
     @Test
     fun `testa 200`() {
-        assertEquals("two hundred", numberNames(200))
+        assertEquals("two hundred", testNames.numberNames(200))
     }
     @Test
     fun `testa 101`() {
-        assertEquals("one hundred and one", numberNames(101))
+        assertEquals("one hundred and one", testNames.numberNames(101))
     }
     @Test
     fun `testa 102`() {
-        assertEquals("one hundred and two", numberNames(102))
+        assertEquals("one hundred and two", testNames.numberNames(102))
     }
     @Test
     fun `testa 110`() {
-        assertEquals("one hundred and ten", numberNames(110))
+        assertEquals("one hundred and ten", testNames.numberNames(110))
     }
     @Test
     fun `testa 111`() {
-        assertEquals("one hundred and eleven", numberNames(111))
+        assertEquals("one hundred and eleven", testNames.numberNames(111))
     }
     @Test
     fun `testa 121` () {
-        assertEquals("one hundred and twenty one", numberNames(121))
+        assertEquals("one hundred and twenty one", testNames.numberNames(121))
     }
     @Test
     fun `testa 8` () {
-        assertEquals("eight", numberNames(8))
+        assertEquals("eight", testNames.numberNames(8))
     }
     @Test
     fun `testa 17` () {
-        assertEquals("seventeen", numberNames(17))
+        assertEquals("seventeen", testNames.numberNames(17))
     }
     @Test
     fun `testa 36` () {
-        assertEquals("thirty six", numberNames(36))
+        assertEquals("thirty six", testNames.numberNames(36))
     }
     @Test
     fun `testa 440` () {
-        assertEquals("four hundred and forty", numberNames(440))
+        assertEquals("four hundred and forty", testNames.numberNames(440))
     }
     @Test
     fun `testa 999` () {
-        assertEquals("nine hundred and ninety nine", numberNames(999))
+        assertEquals("nine hundred and ninety nine", testNames.numberNames(999))
     }
 }
